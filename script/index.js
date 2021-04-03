edf9c3ea093f052d4eb4ed5bc1d1f0b594d43891
Talita Spadaccini <61871070+talitaspadaccini@users.noreply.github.com>
2021-04-03 03:38:07 -0300

    Validação completa do formulário - Validação completa dos inputs e do selecionar - Máscara para moeda no input de valor

--- script/index.js
+++ script/index.js
@@ -1,39 +1,42 @@
-let formTransaction = document.getElementById('form-transaction');
-
-/* Assigning the function “validForm()” to the form's “submit” event */
-if (formTransaction.addEventListener) {                   
-  formTransaction.addEventListener("submit", validForm);
-/* For I.E. 8 and its predecessors it is necessary to call a proprietary method attachEvent() */
-} else if (formTransaction.attachEvent) {                  
-  formTransaction.attachEvent("onsubmit", validForm);
+/* validForm() function where all checks will be made, this function is assigned to the form's “submit” event */
+function validForm(frm) {
+  /* Validating the Transaction Type field - user must choose one of the options */
+  if (frm.typeTransaction.value === 'selection') {
+    document.querySelector('.msg-option').innerHTML = "Por favor, selecione uma opção.";
+    frm.typeTransaction.focus();
+    return false;
+  }
+  /* Validating the Merchandise Name field - user must fill in the field using at least 3 characters */ 
+  if (frm.nameCommodity.value === "" || frm.nameCommodity.value === null || frm.nameCommodity.value.length < 3) {
+    document.querySelector('.msg-commodity').innerHTML = "Por favor, preencha o campo corretamente.";
+    frm.nameCommodity.focus();
+    return false;
+  } 
+  if (frm.valueInput.value === "" || frm.valueInput.value === null) {
+    document.querySelector('.msg-value').innerHTML = "Por favor, preencha o campo com o valor correto.";
+    frm.valueInput.focus();
+    return false;
+}
 }
 
-/* validForm() function where all checks will be made, this function is assigned to the form's “submit” event */
-function validForm(event) {
-  let nameCommodity = document.getElementById('commodity-name').value;
-  
-  let countError = 0;
+/* Function to standardize writing of values as currency. */
+function valueFormated(frm) {
+  let element = document.getElementById('value-input');
+  let valueOption = element.value;
+  valueOption = valueOption + '';
+  valueOption = parseInt(valueOption.replace(/[\D]+/g, ''));
+  valueOption = valueOption + '';
+  valueOption = valueOption.replace(/([0-9]{2})$/g, ",$1");
 
-/* Input commodity name validation */
-  
-  /* Method used to capture the elements where the error messages will be displayed, classes used in the <span> tag below the inputs */
-  let inputNameCommodity = document.querySelector('.msg-commodity'); // document.querySelector() method returns the first element found with the same class name
-  if (nameCommodity === "" || nameCommodity === null) {
-    alert('erro'); // teste para validação
-    inputNameCommodity.innerHTML = "Preencher o campo nome";
-    inputNameCommodity.style.display = 'block';
-    /* If it is detected that the value of a field is invalid then increase the value of the countError variable */
-    countError += 1;
-  } else {
-    inputNameCommodity.style.display = "none";
+  if (valueOption.length > 6) {
+    valueOption = valueOption.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
   }
+  if (valueOption.length > 9) {
+  valueOption = valueOption.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2})$/g, '.$1.$2,$3');
+}
+  
+  element.value = valueOption;
+  if(valueOption === 'NaN') element.value = '';
 
-/* Input commodity value validation */
-  // vazio
   
-  /* If countError> 0, then an error was found and the default behavior of the form of submitting the page for recording should be canceled, 
-  for this we call the event.preventDefault () method. If the value of the function is zero then submission occurs naturally. */
-  if (countError > 0) {
-    event.preventDefault();
-  }
 }
