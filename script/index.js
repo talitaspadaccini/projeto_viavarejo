3439a4154f42851c5294cedc51a44b5d23ab98da
Talita Spadaccini <61871070+talitaspadaccini@users.noreply.github.com>
2021-04-06 00:41:27 -0300

    Aprimora validação do formulário e inicia local storage - Validação do formulário desconfigurou estilo do desktop e foi arrumado - Validação do formulário estava com mensagem fixa que não apagava após preencher o campo; foi resolvido - Implantando local storage, testando funcionalidade

--- script/index.js
+++ script/index.js
@@ -1,42 +1,89 @@
 /* validForm() function where all checks will be made, this function is assigned to the form's “submit” event */
 function validForm(frm) {
+  let countError = 0;
+
   /* Validating the Transaction Type field - user must choose one of the options */
-  if (frm.typeTransaction.value === 'selection') {
-    document.querySelector('.msg-option').innerHTML = "Por favor, selecione uma opção.";
+  let caixaMsgOption = document.querySelector(".msg-option");
+  if (frm.typeTransaction.value === "selection") {
+    caixaMsgOption.innerHTML = "Por favor, selecione uma opção.";
     frm.typeTransaction.focus();
+    caixaMsgOption.style.display = "block";
+    countError += 1;
     return false;
+  } else {
+    caixaMsgOption.style.display = "none";
   }
-  /* Validating the Merchandise Name field - user must fill in the field using at least 3 characters */ 
-  if (frm.nameCommodity.value === "" || frm.nameCommodity.value === null || frm.nameCommodity.value.length < 3) {
-    document.querySelector('.msg-commodity').innerHTML = "Por favor, preencha o campo corretamente.";
+
+  /* Validating the Commodity Name field - user must fill in the field using at least 3 characters */
+  let caixaMsgCommodity = document.querySelector(".msg-commodity");
+  if (
+    frm.nameCommodity.value === "" ||
+    frm.nameCommodity.value === null ||
+    frm.nameCommodity.value.length < 3
+  ) {
+    caixaMsgCommodity.innerHTML = "Por favor, preencha o campo corretamente.";
     frm.nameCommodity.focus();
+    caixaMsgCommodity.style.display = "block";
+    countError += 1;
     return false;
-  } 
+  } else {
+    caixaMsgCommodity.style.display = "none";
+  }
+
+  /* Validating the Commodity Value field */
+  let caixaMsgValue = document.querySelector(".msg-value");
   if (frm.valueInput.value === "" || frm.valueInput.value === null) {
-    document.querySelector('.msg-value').innerHTML = "Por favor, preencha o campo com o valor correto.";
+    caixaMsgValue.innerHTML = "Por favor, preencha o campo com o valor correto.";
     frm.valueInput.focus();
+    caixaMsgValue.style.display = "block";
+    countError += 1;
     return false;
-}
+  } else {
+    caixaMsgValue.style.display = "none";
+  }
+
+  if(countError > 0){
+		evt.preventDefault();
+	}
+
+  registerProduct();
 }
 
 /* Function to standardize writing of values as currency. */
 function valueFormated(frm) {
-  let element = document.getElementById('value-input');
+  let element = document.getElementById("value-input");
   let valueOption = element.value;
-  valueOption = valueOption + '';
-  valueOption = parseInt(valueOption.replace(/[\D]+/g, ''));
-  valueOption = valueOption + '';
+  valueOption = valueOption + "";
+  valueOption = parseInt(valueOption.replace(/[\D]+/g, ""));
+  valueOption = valueOption + "";
   valueOption = valueOption.replace(/([0-9]{2})$/g, ",$1");
 
   if (valueOption.length > 6) {
     valueOption = valueOption.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
   }
   if (valueOption.length > 9) {
-  valueOption = valueOption.replace(/([0-9]{3}).([0-9]{3}),([0-9]{2})$/g, '.$1.$2,$3');
-}
-  
+    valueOption = valueOption.replace(
+      /([0-9]{3}).([0-9]{3}),([0-9]{2})$/g,
+      ".$1.$2,$3"
+    );
+  }
   element.value = valueOption;
-  if(valueOption === 'NaN') element.value = '';
-
-  
+  if (valueOption === "NaN") element.value = "";
 }
+
+function registerProduct(frm) {
+  let dataForm = [];
+  
+  let commodityReg = document.getElementById("commodity-name").value;
+  let valueReg = document.getElementById("value-input").value;
+  
+  dataForm.push(commodityReg, valueReg);
+  let objCommJSON = JSON.stringify(dataForm);
+  localStorage.setItem("formJSON", objCommJSON);
+  
+  getCommJSON = localStorage.getItem("formJSON");
+  parseCommJSON = JSON.parse(getCommJSON);
+
+  addTable();
+}
+
