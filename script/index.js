05c0af2a9196bd2fab5245edef6609a34c097f14
Talita Spadaccini <61871070+talitaspadaccini@users.noreply.github.com>
2021-04-07 11:16:39 -0300

    Reajuste de estilo e HTML para tabela, ajuste validação do formulário - Edição da tabela no HTML e CSS para inserção dos dados via Local Storage - Validação do formulário dando conflito, foi arrumado -* Ajuste do Local Storage, ainda em andamento, não está puxando dados

--- script/index.js
+++ script/index.js
@@ -1,58 +1,59 @@
-/* validForm() function where all checks will be made, this function is assigned to the form's “submit” event */
+// Global Vars
+let elementValue = document.getElementById("value-input");
+let commodityRecord = document.getElementById("commodity-name").value;
+
+// validForm() function where all checks will be made, this function is assigned to the form's “submit” event
 function validForm(frm) {
+  // Variable countError will be incremented, if a field's value is invalid
   let countError = 0;
 
-  /* Validating the Transaction Type field - user must choose one of the options */
-  let caixaMsgOption = document.querySelector(".msg-option");
+  // Validating the Transaction Type field - user must choose one of the options */
+  let optionErrorMsgBox = document.querySelector(".msg-option");
   if (frm.typeTransaction.value === "selection") {
-    caixaMsgOption.innerHTML = "Por favor, selecione uma opção.";
+    optionErrorMsgBox.innerHTML = "Por favor, selecione uma opção.";
     frm.typeTransaction.focus();
-    caixaMsgOption.style.display = "block";
+    optionErrorMsgBox.style.display = "block";
+    
     countError += 1;
     return false;
   } else {
-    caixaMsgOption.style.display = "none";
+    optionErrorMsgBox.style.display = "none";
   }
 
-  /* Validating the Commodity Name field - user must fill in the field using at least 3 characters */
-  let caixaMsgCommodity = document.querySelector(".msg-commodity");
-  if (
-    frm.nameCommodity.value === "" ||
-    frm.nameCommodity.value === null ||
-    frm.nameCommodity.value.length < 3
-  ) {
-    caixaMsgCommodity.innerHTML = "Por favor, preencha o campo corretamente.";
+  // Validating the Commodity Name field - user must fill in the field using at least 3 characters */
+  let commodityErrorMsgBox = document.querySelector(".msg-commodity");
+  if (frm.nameCommodity.value === "" || frm.nameCommodity.value === null || frm.nameCommodity.value.length < 3) {
+    commodityErrorMsgBox.innerHTML = "Por favor, preencha o campo corretamente.";
     frm.nameCommodity.focus();
-    caixaMsgCommodity.style.display = "block";
+    commodityErrorMsgBox.style.display = "block";
+    
     countError += 1;
     return false;
   } else {
-    caixaMsgCommodity.style.display = "none";
+    commodityErrorMsgBox.style.display = "none";
   }
 
-  /* Validating the Commodity Value field */
-  let caixaMsgValue = document.querySelector(".msg-value");
+  // Validating the Commodity Value field */
+  let valueErrorMsgBox = document.querySelector(".msg-value");
   if (frm.valueInput.value === "" || frm.valueInput.value === null) {
-    caixaMsgValue.innerHTML = "Por favor, preencha o campo com o valor correto.";
+    valueErrorMsgBox.innerHTML = "Por favor, preencha o campo com o valor correto.";
     frm.valueInput.focus();
-    caixaMsgValue.style.display = "block";
+    valueErrorMsgBox.style.display = "block";
+    
     countError += 1;
     return false;
   } else {
-    caixaMsgValue.style.display = "none";
+    valueErrorMsgBox.style.display = "none";
   }
-
-  if(countError > 0){
-		evt.preventDefault();
-	}
-
-  registerProduct();
+  // If countError> 0, do not submit the page for recording, using the evt.preventDefault() method
+  if (countError > 0) {
+    evt.preventDefault();
+  }
 }
 
-/* Function to standardize writing of values as currency. */
+// Function to standardize writing of values as currency. */
 function valueFormated(frm) {
-  let element = document.getElementById("value-input");
-  let valueOption = element.value;
+  let valueOption = elementValue.value;
   valueOption = valueOption + "";
   valueOption = parseInt(valueOption.replace(/[\D]+/g, ""));
   valueOption = valueOption + "";
@@ -67,23 +68,42 @@ function valueFormated(frm) {
       ".$1.$2,$3"
     );
   }
-  element.value = valueOption;
-  if (valueOption === "NaN") element.value = "";
+  elementValue.value = valueOption;
+  if (valueOption === "NaN") elementValue.value = "";
 }
 
-function registerProduct(frm) {
-  let dataForm = [];
-  
-  let commodityReg = document.getElementById("commodity-name").value;
-  let valueReg = document.getElementById("value-input").value;
-  
-  dataForm.push(commodityReg, valueReg);
-  let objCommJSON = JSON.stringify(dataForm);
-  localStorage.setItem("formJSON", objCommJSON);
-  
-  getCommJSON = localStorage.getItem("formJSON");
-  parseCommJSON = JSON.parse(getCommJSON);
 
-  addTable();
+// Function that will store the data entry in the local storage
+function registerProduct() {
+  let arrayFormData = [];
+  
+  // Add the information to the array
+  let pullData = {
+    commodityRecord: commodityRecord,
+    elementValue: elementValue
+  };
+
+  arrayFormData.push(pullData);
+
+  // Save the changed list
+  localStorage.setItem("arrayJSON", JSON.stringify(arrayFormData));
+
+  // Get the list already registered, if there is none, it turns into an empty array
+  arrayFormData = JSON.parse(localStorage.getItem("arrayJSON"));
 }
+/*
+// Function that adds the data captured in the local storage to the table
+function addTable(arrayFormData) {
+  let tableRef = document.getElementById("table-container").getElementsByTagName("tbody")[0];
 
+  let tr = document.createElement("tr");
+  let tdSinal = document.createElement("td");
+  let tdCommodity = document.createElement("td");
+  let tdValue = document.createElement("td");
+
+  tdCommodity.textContent = arrayFormData.commodityRecord;
+  tdValue.textContent = arrayFormData.valueRecord;
+
+  tr.appendChild(tdCommodity);
+  tr.appendChild(tdValue);
+}*/
